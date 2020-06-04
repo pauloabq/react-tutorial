c9116550c97e85ff24c68029219705b5ea483500
pauloabq <pcajunior@gmail.com>
2020-06-03 22:40:00 -0300

    Updates

---
+++ precache-manifest.f317fccdd5d8a13eaf269cdb4ff2727d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da62fea362cf6acfb077d642c4818c6",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "b6b71c5410fb7588e810",
+    "url": "/react-tutorial/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "7599bf2db974bd532148",
+    "url": "/react-tutorial/static/js/2.1a0425fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.1a0425fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b71c5410fb7588e810",
+    "url": "/react-tutorial/static/js/main.d84f45eb.chunk.js"
+  },
+  {
+    "revision": "658311ebffd84fed121f",
+    "url": "/react-tutorial/static/js/runtime-main.5dd50962.js"
+  }
+]);
